1c89ee106a43f99addc61f2df78557f116256f09
dipakpratale158 <112299138+dipakpratale158@users.noreply.github.com>
2023-04-17 01:51:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Products from "./components/Products";
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <div>
+      <h1>Shoe Commerce Platform</h1>
+      <Navbar />
+      <Products />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+         <a href="Home">Home</a>
+        </li>
+        <li>
+        <a href="Store">Store</a>
+        </li>
+        <li>
+        <a href="About">About</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
